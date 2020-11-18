c78474ef40dc7671e2d715c2981cc23bdf690f96
JyothiNandyala <njyothi451@gmail.com>
2020-11-18 16:55:49 +0530

    remove extra files

--- git/week1/humanage.js
+++
@@ -1,6 +0,0 @@
-const yearOfBirth =1992;
-let yearFuture=2045;
-console.log(typeof yearOfBirth);
-console.log(typeof yearFuture);
-let age=yearFuture-yearOfBirth;
-console.log("You will be "+ age + " years old in "+yearFuture);
--- git/week1/startupname.js
+++
@@ -1,8 +0,0 @@
-let firstWords=["Easy","Awesome","Corporate","ToneRose","super","GrayMore","Brutaller","Voolzie","DoomDust","good"];
-let secondWords=["brillent","verygood","extraordinary","jems","hard","bad","worest","Lovelypot","Hipsterion","Prophicient"];
-let startupName ;
-const randomNumber = Math.floor(Math.random(startupName) * 10) + 0
-startupName=firstWords[randomNumber] +' '+ secondWords[randomNumber];
-console.log("The startup: "  + startupName +  " contains " + startupName.length + " characters.");
-
-
