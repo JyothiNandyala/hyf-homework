9a20090f89a106e82687563e748af0da768db1ff
JyothiNandyala <njyothi451@gmail.com>
2020-11-19 15:00:13 +0530

    modified the changes in to the file.

--- javascript/javascript1/week3/seriesduration.js
+++ javascript/javascript1/week3/seriesduration.js
@@ -21,15 +21,15 @@ const seriesDurations = [
     },
   ];
  
-  const lifespanHours=700800;
+  const lifeSpanHours=700800;
 let totalPercentage=0;
   
  function logOutSeriesText(){
     
     for(let  i=0;i<seriesDurations.length;i++){
-        let totalHours=seriesDurations[i].days*24+seriesDurations[i].hours+(seriesDurations[i].minutes)/60;
+        const totalHours=seriesDurations[i].days*24+seriesDurations[i].hours+(seriesDurations[i].minutes)/60;
         
-        const averagePercentage=(totalHours/lifespanHours)*100;
+        const averagePercentage=(totalHours/lifeSpanHours)*100;
 
         console.log(seriesDurations[i].title + " took "+ averagePercentage.toFixed(3)+ " % of my life") ;
         totalPercentage=totalPercentage+averagePercentage;
