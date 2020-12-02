7ca02b605838187597cabc5d10e16f88c094713d
JyothiNandyala <njyothi451@gmail.com>
2020-12-02 06:13:39 +0530

    changes have maded.

--- javascript/javascript1/week4/voiceassistent.js
+++ javascript/javascript1/week4/voiceassistent.js
@@ -1,104 +1,106 @@
 "use strict"
+const todo = [];
+let name = "";
 
-//voice Assistant
-let name='';
-let todos=[];
+function getReply(commandArray) {
+  if (typeof commandArray != "string") {
+    return "Please enter your command as a string which means in quotes";
+  }
+  const commandWords = commandArray.split(" ");
+  const elementActivity = commandArray.substring(3, commandArray.length - 11);
 
-function getReply(command){
-    const commandArray=command.split(' ');
-    
-
-    if(typeof command!=="string"){
-        return "please enter the command in specific formate";
+  if (commandArray) {
+     const number1 = commandWords[2];
+    const number2 = commandWords[4];
+    let result;
+    if (commandWords[3] === "+") {
+        result = number1 + number2;
+      return  result;
+    } else if (commandWords[3] === "-") {
+        result = number1 - number2;
+      return  result;
+    } else if (commandWords[3] === "/") {
+        result = number1 / number2;
+      return  result;
+    } else if (commandWords[3] === "*") {
+        result = number1 * number2;
+      return  result;
     }
+  }
+  switch (commandArray) {
+    case "Hello my name is jyothi":
+      if (name == "") {
+        name = commandWords[commandWords.length - 1];
+        return "Nice to meet you " + name;
+      } else {
+        return "Your name " + name + " already there";
+      }
+      break;
+    case "What is my name?":
+      if (name !== undefined) {
+        return "Your name is " + name;
+      } else {
+        return "Please enter your name first.";
+      }
+      break;
+    case "Add fishing to my todo":
+      todo.push(elementActivity);
+      return elementActivity + " added to your todo.";
+      break;
+    case "Add singing in the shower to my todo":
+      todo.push(elementActivity);
+      return elementActivity + " added to your todo.";
+      break;
     
-    else if(commandArray[0]==="hello"){
-        if(name===''){
-           name= commandArray[commandArray.length-1];
-        return "Nice to meet you " +name;
-    }else{
-        return "your name already there";
-    }
-}
-
-else if(commandArray[0]==="what"){
-    return "Your name is " +name;
-
-}
-else if(command.includes("add") && command.includes("todo")){
-const addtodo=commandArray.slice(1, 2).join(" ");
-
-todos.push(addtodo);
-return addtodo+ " added to your todo";
-}
-
-else if(command.includes("Remove") && command.includes("todo")){
-    const addtodo=commandArray.slice(1,-3).join(" ");
-if(todos.includes("addtodo")){
-    todos.splice(todos.indexOf(addtodo));
-    return addtodo+ " removed fishing from your todo";
-}else{
-    return addtodo+ " is not in the todos list";
-}
-}
-else if(command==="What is on my todo?"){
-    return todos;
-}
-else if(command==="What day is it today?"){
-    let date=new Date();
-    let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const response=date.getDate() + " of " + months[date.getMonth()]+ ' ' +date.getFullYear();
-return response;
-}
-if(commandArray[0]==="Set"){  
-
-    const minutes = commandArray[4];
-    const milliSeconds = minutes*60*1000;
-
-
-    function setTimer(){
-        console.log("Timer done");
-}
-    console.log("Timer set for "+commandArray[4]+ " " +commandArray[5]);
-    setTimeout(setTimer, milliSeconds);
-    return "Timer is set so just wait";
-}
-if(command){
-    return getMathFunction(commandArray);
-}
-}
-//Mathamatics it will calculate the mathematics operation and return the result.
-
-function getMathFunction(commandArray){
-    let  operation=commandArray[3];
-    let a=parseInt(commandArray[2]);
-    let b=parseInt(commandArray[4]);
     
-        switch(operation){
-            case '+':
-                return a+b;   
-                 break;
-            case '-':
-                     return a-b;
-                     break;
-             case '*':
-                 return a*b;
-                 break; 
-            case '/':
-                     return a/b;
-                     break;   
-        }
+    case "Remove singing in the shower from my todo":
+      return "Removed" + removeElement(commandArray) + " from your todo.";
+      break;
+    case "What is on my todo?":
+      return "you have " + todo.length + " todos - " + todo;
+      break;
+    case "What day is it today?":
+        let date=new Date();
+        let months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const response=date.getDate() + " of " + months[date.getMonth()]+ ' ' +date.getFullYear();
+    return response;
+      break;
+      
+    case "Set a timer for 4000 milliseconds":
+        const minutes = commandArray[4];
+        const milliSeconds = minutes*60*1000;
+    
+     function setTimer(){
+            console.log("Timer done");
     }
+        console.log("Timer set for "+commandArray[4]+ " " +commandArray[5]);
+        setTimeout(setTimer, milliSeconds);
+        return "Timer is set so just wait";
+        break;
+    default:
+      return "please enter the required format command";
+  }
+  
+  function removeElement(inputString) {
+    const removeElementFromTODo = inputString.substring(6, inputString.length - 13);
+    for (let i = 0; i < todo.length; i++) {
+      if (todo[i] === removeElementFromTODo) {
+        return todo.splice(i, 1);
+      }
+    }
+  }
+}
 
 
-console.log(getReply("hello my name is Benjamin")); // "Nice to meet you benjamin"
-console.log(getReply("hello my name is Benjamin"));
-console.log(getReply("what is my name?")); // "Your name is Benjamin"
-console.log(getReply("add fishing to my todo")); // "fishing added to your todo"
-console.log(getReply("add singing in the shower to my todo"));
-console.log(getReply("Remove fishing from my todo"));
+console.log(getReply("Hello my name is jyothi"));
+console.log(getReply("What is my name?"));
+console.log(getReply("Add fishing to my todo"));
+console.log(getReply("Add singing in the shower to my todo"));
+console.log(getReply("Remove singing in the shower from my todo"));
 console.log(getReply("What is on my todo?"));
 console.log(getReply("What day is it today?"));
-console.log(getReply("What is 3 + 3"));
-console.log(getReply("What is 4 * 12"));
-console.log(getReply("Set a timer for 1 minute"));
+console.log(getReply("what is 3 + 3"));
+console.log(getReply("what is 12 - 2"));
+console.log(getReply("what is 10 / 5"));
+console.log(getReply("what is 4 * 12"));
+console.log(getReply("Set a timer for 4000 milliseconds"));
